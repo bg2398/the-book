6a29b1a9308e798743345bc11712f4df6d5b4b05
Brandon Guerrero <brandon_g909@yahoo.com>
2023-05-13 20:18:08 -0700

    finsished code updated

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,14 @@
+const mongoose = require("mongoose");
 
+mongoose.connect(  // Url for your local mongoose host below
+  process.env.MONGODB_URI || "mongodb://localhost:27017/the-book",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+
+mongoose.set("debug", true);
+
+module.exports = mongoose.connection;
